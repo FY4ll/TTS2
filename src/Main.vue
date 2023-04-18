01585b04199f8e983bea977507b2405f3e7ab951
FY4ll <fadyallemann@gmail.com>
2023-04-18 08:44:00 +0200

    BACKED UP

--- src/Main.vue
+++ src/Main.vue
@@ -4,22 +4,21 @@ import Profile from "@/Profile.vue";
 import { store } from './store.vue'
 import axios from 'axios';
 export default {
-
     components: {Profile},
-  data() {
-    return {
-      msalConfig: {
-        auth: {
-          clientId: "",
-          authority: "",
-          redirectUri: "",
-        },
-      },
-      msalInstance: null,
-      userInfo: null, // nouvelle variable pour stocker les informations de l'utilisateu
-        store
-    };
-  },
+    data() {
+        return {
+            msalConfig: {
+                auth: {
+                    clientId: "a7bb9be7-109d-4c12-9f58-b977f84a3700",
+                    authority: "https://login.microsoftonline.com/de084535-12a9-406c-bd84-d4bbcdb1a7b4",
+                    redirectUri: "http://localhost:5173",
+                },
+            },
+            msalInstance: null,
+            userInfo: null, // nouvelle variable pour stocker les informations de l'utilisateu
+            store
+        };
+    },
     mounted() {
         this.msalInstance = new msal.PublicClientApplication(this.msalConfig);
         const account = this.msalInstance.getAllAccounts()[0];
@@ -31,16 +30,16 @@ export default {
     methods: {
         async signIn() {
             if(store.userInfo == null){
-              try {
-                  const loginRequest = {
-                      scopes: ["openid", "profile", "User.Read"],
-                  };
-                  const authResult = await this.msalInstance.loginPopup(loginRequest);
-                  this.msalInstance.setActiveAccount(authResult.account);
-                  await this.getUserInfo();
-              } catch (error) {
-                  console.log(error);
-              }
+                try {
+                    const loginRequest = {
+                        scopes: ["openid", "profile", "User.Read"],
+                    };
+                    const authResult = await this.msalInstance.loginPopup(loginRequest);
+                    this.msalInstance.setActiveAccount(authResult.account);
+                    await this.getUserInfo();
+                } catch (error) {
+                    console.log(error);
+                }
             }
         },
         async getUserInfo() {
@@ -82,56 +81,55 @@ export default {
             }
         }
     }
-  }
+}
 </script>
 <template>
-  <div class="Arrow">
-    <router-link to="/profile" @click="signIn">Profile</router-link>
-    <img src="./IMG/Arrow%20Left.svg">
-  </div>
-  <div class="main_logo">
-    <img src="./IMG/image.png" alt="MAIN">
-  </div>
-  <div class="Arrow">
-    <router-link v-if="store.userInfo != null" to="/scene">Scene</router-link>
-      <a v-else @click="signIn">Scene</a>
-    <img src="./IMG/Arrow%20Right.svg">
-  </div>
+    <div class="Arrow">
+        <router-link to="/profile" @click="signIn">Profile</router-link>
+        <img src="./IMG/Arrow%20Left.svg">
+    </div>
+    <div class="main_logo">
+        <img src="./IMG/image.png" alt="MAIN">
+    </div>
+    <div class="Arrow">
+        <router-link v-if="store.userInfo != null" to="/scene">Scene</router-link>
+        <a v-else @click="signIn">Scene</a>
+        <img src="./IMG/Arrow%20Right.svg">
+    </div>
 </template>
 <style scoped>
 @font-face {
-  font-family: 'Bebas Nueue';
-  src: url('src/assets/fonts/Bebas_Neue/BebasNeue-Regular.ttf') format('truetype');
+    font-family: 'Bebas Nueue';
+    src: url('src/assets/fonts/Bebas_Neue/BebasNeue-Regular.ttf') format('truetype');
 }
 #app{
-  display: grid;
-  grid-template-columns: repeat(3,1fr);
-  margin-top: 100%;
+    display: grid;
+    grid-template-columns: repeat(3,1fr);
+    margin-top: 100%;
 }
 a{
-  font-size: 64px;
-  color:#8873B6;
-  font-family: "Bebas Nueue",sans-serif ;
+    font-size: 64px;
+    color:#8873B6;
+    font-family: "Bebas Nueue",sans-serif ;
 }
 a:link{
-  text-decoration: none;
+    text-decoration: none;
 }
-
 div{
-  display: flex;
-  justify-content: center;
+    display: flex;
+    justify-content: center;
 }
 .main_logo img{
-  margin-top: 30%;
+    margin-top: 30%;
 }
 .Arrow{
-  display: grid;
-  grid-template-rows: repeat(2,1fr);
+    display: grid;
+    grid-template-rows: repeat(2,1fr);
 }
 .Arrow a{
-  display: flex;
-  margin-top: 100%;
-  align-items: center;
-  justify-content: center;
+    display: flex;
+    margin-top: 100%;
+    align-items: center;
+    justify-content: center;
 }
 </style>
